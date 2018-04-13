892dbacb22bb17b2e1068f23b99f29d6c31df185
Pramod Pradhan <ppradhan@VENTERA.com>
2018-04-13 09:36:16 -0400

    Initial Push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { AuthorComponent } from './admin/author/author.component';
+import { AuthorListComponent } from './admin/author-list/author-list.component';
+import { AuthorService } from './admin/author.service';
+import { HttpModule } from '@angular/http';
+import { FormsModule} from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    AuthorComponent,
+    AuthorListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [AuthorService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
